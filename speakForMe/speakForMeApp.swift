e9474d861a10da220bf979b0b3fc395ea09b14c7
Jameka Echols <jameka.echols@gmail.com>
2026-06-13 21:33:07 -0400

    Initial Commit

---
+++ speakForMe/speakForMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  speakForMeApp.swift
+//  speakForMe
+//
+//  Created by Jameka Echols on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct speakForMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ speakForMeTests/speakForMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  speakForMeTests.swift
+//  speakForMeTests
+//
+//  Created by Jameka Echols on 6/13/26.
+//
+
+import Testing
+@testable import speakForMe
+
+struct speakForMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
