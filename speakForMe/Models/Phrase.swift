69fb6e02aab828b0ef0df0cad524c699dab0ce96
Jameka Echols <jameka.echols@gmail.com>
2026-06-14 12:27:06 -0400

    added models"

---
+++ speakForMe/Models/Phrase.swift
@@ -0,0 +1,17 @@
+//
+//  Phrase.swift
+//  speakForMe
+//
+//  Created by Jameka Echols on 6/14/26.
+//
+
+import Foundation
+
+struct Phrase {
+    var id: Int
+    var text: String
+    var createdAt: Date
+    var lastUsedAt: Date
+    var useCount: Int
+    var category: String
+}
